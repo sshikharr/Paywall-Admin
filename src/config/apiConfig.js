3ea68b1970660054c43385a6b74ef8341737c889
sshikharr <shikhardwivedi2019@gmail.com>
2025-07-11 14:38:18 +0530

    URL changed

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,2 +1,2 @@
-export const baseUrl = 'http://localhost:5001/api';
-// export const baseUrl = `https://paywall-backend-tkxq.onrender.com/api`
+// export const baseUrl = 'http://localhost:5001/api';
+export const baseUrl = `https://paywall-backend-tkxq.onrender.com/api`
